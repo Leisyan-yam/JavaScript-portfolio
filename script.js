705c86faa8b8cd38395254989d8bdd047fc0fa27
Leisyan Yamaltdinova <n.leisyan@yandex.ru>
2022-09-27 18:10:06 +0300

    first commit

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 
-// function toggleMobileMenu(menu) {
-//     menu.classList.toggle('open')
-// }
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open')
+}
 
-// gsap.from('nav', {opacity:0, duration: 2, stagger: 1} ) 
-// gsap.from('h1', {opacity:0, duration: 2, stagger: 1} )
+gsap.from('nav', {opacity:0, duration: 2, stagger: 1} ) 
+gsap.from('h1', {opacity:0, duration: 2, stagger: 1} )
 
 
-// gsap.to(".greeting",{
-//     text: "Hi there! My name is Leisyan. I am Moscow based Front End developer.",
-//     duration: 8,
-//     ease: "power1",
-//     delay: 2
-//     }) 
+gsap.to(".greeting",{
+    text: "Hi there! My name is Leisyan. I am Moscow based Front End developer.",
+    duration: 8,
+    ease: "power1",
+    delay: 2
+    }) 
 
-const items = document.querySelectorAll('.sert');
-items.forEach(item => {
-    item.addEventListener('mouseover',()=> {
-item.classList.add('selected')
-    })
-    removeFocus = () => {
-      items.forEach(item => {
-          item.classList.remove('selected')
-      })
-  }
-})
+
+
+
+
     
 
